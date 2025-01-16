8ee078e9ded79acfb50732fccfa9b521c8dfbb5e
Valdecy Pereira <valdecypereira@yahoo.com.br>
2025-01-15 21:53:19 -0300

    Update util.py

--- pyCombinatorial/utils/util.py
+++ pyCombinatorial/utils/util.py
@@ -49,4 +49,31 @@ def build_distance_matrix(coordinates):
    b = a.reshape(np.prod(a.shape[:-1]), 1, a.shape[-1])
    return np.sqrt(np.einsum('ijk,ijk->ij',  b - a,  b - a)).squeeze()
 
+# Function: LatLong -> Cartesian
+def latlong_to_cartesian(lat_long):
+    if (hasattr(lat_long, 'values')): 
+        lat_long = lat_long.values
+    lat     = lat_long[:, 0]
+    lon     = lat_long[:, 1]
+    lat_rad = np.radians(lat)
+    lon_rad = np.radians(lon)
+    R       = 6371.0
+    x       = R * np.cos(lat_rad) * np.cos(lon_rad)
+    y       = R * np.cos(lat_rad) * np.sin(lon_rad)
+    #z       = R * np.sin(lat_rad)
+    return np.column_stack((x, y))
+
+# Function: Build Distance Matrix Lat Long
+def latlong_distance_matrix(coordinates):
+    coords_rad = np.radians(coordinates)
+    coords_rad = coords_rad.values
+    latitudes  = coords_rad[:, 0][:, np.newaxis]
+    longitudes = coords_rad[:, 1][:, np.newaxis]
+    dlat       = latitudes - latitudes.T
+    dlon       = longitudes - longitudes.T
+    a          = np.sin(dlat / 2)**2 + np.cos(latitudes) * np.cos(latitudes.T) * np.sin(dlon / 2)**2
+    c          = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
+    R          = 6371.0
+    return R * c
+
 ############################################################################
