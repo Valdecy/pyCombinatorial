d846105057854e9ac3ee2af31074472441311e98
Valdecy Pereira <valdecypereira@yahoo.com.br>
2026-02-14 18:46:04 -0300

    Bump version from 2.1.0 to 2.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name='pycombinatorial',
-    version='2.1.0',
+    version='2.1.1',
     license='GNU',
     author='Valdecy Pereira',
     author_email='valdecy.pereira@gmail.com',
