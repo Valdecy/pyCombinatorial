84afc40a120b64a23d0fea12e33724a39d44d43e
Valdecy Pereira <valdecypereira@yahoo.com.br>
2026-05-07 11:37:27 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='pycombinatorial',
-    version='2.2.2',
+    version='2.2.3',
     license='GNU',
     author='Valdecy Pereira',
     author_email='valdecy.pereira@gmail.com',
